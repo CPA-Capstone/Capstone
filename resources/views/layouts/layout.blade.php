9315ff531ace3885ac81bc7082e558e73a76e9d8
Tyler Querido <tyler.querido@dcmail.ca>
2020-01-22 10:58:43 -0500

    Added welcome page

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Online Database Builder</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/blog/">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/ODB.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="blog-masthead">
+      <nav class="nav blog-nav">
+        <h4 class="nav-header">Online Database Builder</h4>
+      </nav>
+    </div>
+
+    <div class="container">
+
+      <div class="row">
+
+      	@yield ('content')
+        
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <footer class="blog-footer">
+      <p>&copy;Online Database Builder</p>
+    </footer>
+  </body>
+</html>
